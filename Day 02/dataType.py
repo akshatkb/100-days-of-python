864b2b00ee6ae3d12207b563d7ec4939922c674f
Akshat <akshatbaranwal01@gmail.com>
2025-12-29 23:36:08 +0530

    day02

---
+++ Day 02/dataType.py
@@ -0,0 +1,19 @@
+# Subscripting
+print("hello"[0])
+
+# string
+print("123" + "345")
+
+# Integer
+print(123 + 345)
+
+# Large Int
+print(123,456,789)
+print(123_456_789)
+
+# Float
+print(3.14243)
+
+# Boolean
+print(True)
+print(False)
